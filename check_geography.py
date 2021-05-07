38ea27389b3624d1454d679eb9c3d709217dbc83
rgball4 <32111768+rgball4@users.noreply.github.com>
2021-05-07 01:25:29 -0400

    Python file to check geography

---
+++ check_geography.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import geopandas as gpd
+import geopy
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+import matplotlib.pyplot as plt
+import plotly_express as px
+import tqdm
+from tqdm._tqdm_notebook import tqdm_notebook
+
+baseball_df = pd.read_json('sprt.dev/server/data/baseball.json')
+basketball_df = pd.read_json('sprt.dev/server/data/basketball.json')
+football_df = pd.read_json('sprt.dev/server/data/football.json')
+hockey_df = pd.read_json('sprt.dev/server/data/hockey.json')
+
+print(baseball_df.head())
