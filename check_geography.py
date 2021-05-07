8e9b3b94a455ac883cf05993b49fec78765a1589
rgball4 <32111768+rgball4@users.noreply.github.com>
2021-05-07 11:53:40 -0400

    fix reading in json

--- check_geography.py
+++ check_geography.py
@@ -8,9 +8,9 @@ import plotly_express as px
 import tqdm
 from tqdm._tqdm_notebook import tqdm_notebook
 
-baseball_df = pd.read_json('sprt.dev/server/data/baseball.json')
-basketball_df = pd.read_json('sprt.dev/server/data/basketball.json')
-football_df = pd.read_json('sprt.dev/server/data/football.json')
-hockey_df = pd.read_json('sprt.dev/server/data/hockey.json')
+baseball_df = pd.read_json('server/data/baseball.json')
+# basketball_df = pd.read_json('sprt.dev/server/data/basketball.json')
+# football_df = pd.read_json('sprt.dev/server/data/football.json')
+# hockey_df = pd.read_json('sprt.dev/server/data/hockey.json')
 
 print(baseball_df.head())
